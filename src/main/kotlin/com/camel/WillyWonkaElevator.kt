8e67037e723dda5f77109f509d42a1ac58b07dd5
Craig Unrein <cunrein@gmail.com>
2023-11-26 12:04:42 -0700

    chore(OA-1): Some clean-up

--- src/main/kotlin/com/camel/WillyWonkaElevator.kt
+++ src/main/kotlin/com/camel/WillyWonkaElevator.kt
@@ -6,6 +6,9 @@ import kotlin.math.sqrt
 /**
  * This is an elevator that can go anyways (up, down, left, right), so it travels from door to door not floor to floor.
  * This implementation also uses a one step greedy algorithm to decide which door to move to.
+ *
+ * Note the interface this inherits from uses the startFloor and floorsToVisit, I would change it to Doors but that
+ * could cause problems with the JVM.
  */
 class WillyWonkaElevator(private val timeBetweenDoors: Int) : Elevator<Pair<Int, Int>> {
 
@@ -16,14 +19,14 @@ class WillyWonkaElevator(private val timeBetweenDoors: Int) : Elevator<Pair<Int,
     override fun run(startFloor: Pair<Int, Int>, floorsToVisit: List<Pair<Int, Int>>): Pair<Int, List<Pair<Int, Int>>> {
         var totalTime = 0.0
         var from = startFloor
-        val floors = mutableListOf(startFloor)
-        val ftv = floorsToVisit.toMutableList()
+        val doors = mutableListOf(startFloor)
+        val dtv = floorsToVisit.toMutableList()
 
-        while (ftv.size > 0) {
+        while (dtv.size > 0) {
             var closestDist = Double.MAX_VALUE
             var closestDoor = Pair(Int.MIN_VALUE, Int.MIN_VALUE)
             var closestIndex = -1
-            ftv.forEachIndexed { i, f ->
+            dtv.forEachIndexed { i, f ->
                 val dist = sqrt((f.first.toDouble() - from.first).pow(2) + (f.second.toDouble() - from.second).pow(2))
                 if (closestDist > dist) {
                     closestDist = dist
@@ -32,15 +35,15 @@ class WillyWonkaElevator(private val timeBetweenDoors: Int) : Elevator<Pair<Int,
                 }
             }
             if (closestIndex != -1) {
-                ftv.removeAt(closestIndex)
+                dtv.removeAt(closestIndex)
                 totalTime += closestDist * timeBetweenDoors
                 from = closestDoor
-                floors.add(closestDoor)
+                doors.add(closestDoor)
             } else {
                 throw IllegalStateException("Invalid data provided.")
             }
 
         }
-        return Pair(totalTime.toInt(), floors.toList())
+        return Pair(totalTime.toInt(), doors.toList())
     }
 }
